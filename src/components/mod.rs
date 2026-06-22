6dce267f1ca867935375da04e3d505ea4d87c52b
Riky Ripaldo <rikyripaldo@icloud.com>
2026-06-22 16:21:50 +0900

    leptos version -- add floating navbar

--- src/components/mod.rs
+++ src/components/mod.rs
@@ -12,7 +12,7 @@ pub fn SiapaYangTau() -> impl IntoView {
       <h1 class="text-6xl font-bold">{"404"}</h1>
       <h3 class="text-3xl font-bold mt-2">{"PAGE_NOT_FOUND"}</h3>
       <p class="text-grey-500 text-sm mt-2">{"Mungkin url yang kamu masukan salah atau typo ?"}</p>
-      <a href="/home" class="mt-3 relative inline-flex items-center gap-2 rounded-2xl px-5 py-3 bg-white/10 hover:bg-white/15 border border-white/10 hover:shadow-[0_0_25px_white]">{"Kembali"}</a>
+      <a href="/" class="mt-3 relative inline-flex items-center gap-2 rounded-2xl px-5 py-3 bg-white/10 hover:bg-white/15 border border-white/10 hover:shadow-[0_0_25px_white]">{"Kembali"}</a>
     </div>
   }
 }
